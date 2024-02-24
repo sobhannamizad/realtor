98af953f3fc7e7fbabc978d2d3d46a256d99c9ae
name <sobhannamizad02@gmail.com>
2024-02-24 10:49:11 +0330

    add cart model

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import User
+from ads.models import Property
 
-# Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='cart')
+    property = models.ForeignKey(Property,on_delete=models.CASCADE)
+    is_paid = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    paid_at = models.DateTimeField(blank=True,null=True)
+
+    def __str__(self):
+        return f'{self.user.full_name}-{self.property.name} - {self.is_paid}'
